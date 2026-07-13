ffecf049a4872abae8d533a4cc16b51c3974095c
tarzankatlam206-hub <tarzankatlam206@gmail.com>
2026-07-13 11:40:46 +0530

    Update AppNavigator.tsx

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -6,6 +6,10 @@ import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import DashboardScreen from "../screens/DashboardScreen";
 import MembersScreen from "../screens/MembersScreen";
+import NoticeScreen from "../screens/NoticeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import FeesScreen from "../screens/FeesScreen";
+import ContactScreen from "../screens/ContactScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,37 +22,18 @@ export default function AppNavigator() {
           backgroundColor: "#0B6E4F",
         },
         headerTintColor: "#fff",
+        headerTitleAlign: "center",
       }}
     >
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ title: "महानदी हार्वेस्टर" }}
-      />
-
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{ title: "लॉगिन" }}
-      />
-
-      <Stack.Screen
-        name="Register"
-        component={RegisterScreen}
-        options={{ title: "सदस्य पंजीकरण" }}
-      />
-
-      <Stack.Screen
-        name="Dashboard"
-        component={DashboardScreen}
-        options={{ title: "डैशबोर्ड" }}
-      />
-
-      <Stack.Screen
-        name="Members"
-        component={MembersScreen}
-        options={{ title: "सदस्य सूची" }}
-      />
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: "होम" }} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{ title: "लॉगिन" }} />
+      <Stack.Screen name="Register" component={RegisterScreen} options={{ title: "सदस्य पंजीकरण" }} />
+      <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ title: "डैशबोर्ड" }} />
+      <Stack.Screen name="Members" component={MembersScreen} options={{ title: "सदस्य सूची" }} />
+      <Stack.Screen name="Notice" component={NoticeScreen} options={{ title: "नोटिस बोर्ड" }} />
+      <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "प्रोफ़ाइल" }} />
+      <Stack.Screen name="Fees" component={FeesScreen} options={{ title: "सदस्यता शुल्क" }} />
+      <Stack.Screen name="Contact" component={ContactScreen} options={{ title: "संपर्क" }} />
     </Stack.Navigator>
   );
 }
